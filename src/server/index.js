b043e56b3c229df24c0fa628ac125425877024dd
Mahmoudbaarakat <80407892+Mahmoudbaarakat@users.noreply.github.com>
2021-04-16 13:22:30 -0700

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -7,10 +7,8 @@ const bodyParser = require('body-parser')
 const fetch = require('node-fetch');
 const mockAPIResponse = require('./mockAPI.js')
 
-// Start up an instance of app
 const app = express()
 
-// Cors allows the browser and server to communicate without any security interruptions
 const cors = require('cors');
 
 app.use(cors());
@@ -21,7 +19,6 @@ app.use(express.static('dist'))
 
 console.log(__dirname)
 
-// API
 const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?'
 const apiKey = process.env.API_KEY
 console.log(`Your API Key is ${process.env.API_KEY}`);
@@ -36,7 +33,6 @@ app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
 
-// POST Route
 app.post('/api', async function(req, res) {
     userInput = req.body.url;
     console.log(`You entered: ${userInput}`);
@@ -46,19 +42,18 @@ app.post('/api', async function(req, res) {
     const mcData = await response.json()
     console.log(mcData)
     res.send(mcData)
-    /** server sends only specified data to the client with below codes
-     * const projectData = {
-     *  score_tag : mcData.score_tag,
-     *  agreement : mcData.agreement,
-     *  subjectivity : mcData.subjectivity,
-     *  confidence : mcData.confidence,
-     *  irony : mcData.irony
-     * }
-     * res.send(projectData);
-     * */
+
+      const projectData = {
+       score_tag : mcData.score_tag,
+       agreement : mcData.agreement,
+       subjectivity : mcData.subjectivity,
+       confidence : mcData.confidence,
+       irony : mcData.irony
+      }
+      res.send(projectData);
 })
 
-// designates what port the app will listen to for incoming requests
+
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
